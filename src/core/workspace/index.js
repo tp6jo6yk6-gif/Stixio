3d922720f494edb9397799bddeccd597d75dce7b
tp6jo6yk6-gif <tp6jo6yk6@gmail.com>
2026-06-27 23:51:49 +0800

    Add workspace module index

---
+++ src/core/workspace/index.js
@@ -0,0 +1,5 @@
+export * from './workspace-schema.js';
+export * from './workspace-manager.js';
+export * from './workspace-storage.js';
+export * from './collection.js';
+export * from './library.js';
