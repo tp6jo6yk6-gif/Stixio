9691f0385fa7daa81195d964ff93a70a1e44b045
tpjoyk666 <tp6jo6yk6@gmail.com>
2026-06-28 12:55:43 +0800

    Finalize Stixio brand module

--- src/core/brand.js
+++ src/core/brand.js
@@ -1,10 +1,14 @@
 export const BRAND = Object.freeze({
   name: 'Stixio',
-  slogan: 'One workspace. Every sticker.',
-  mission: 'Create once. Adapt everywhere.',
-  productCategory: 'Sticker Production Workspace'
+  shortName: 'Stixio',
+  slogan: 'Create once. Adapt everywhere.',
+  mission: 'A fast sticker production workspace for creators.',
+  productCategory: 'Sticker Production Workspace',
+  version: '0.9.0-beta',
+  betaHost: 'beta.stixio.app',
+  productionHost: 'stixio.app'
 });
 
 export function getAppTitle() {
-  return BRAND.name + ' - ' + BRAND.slogan;
+  return `${BRAND.name} - ${BRAND.slogan}`;
 }
